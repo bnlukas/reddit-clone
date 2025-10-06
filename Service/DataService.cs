8c684f743b91e0300dec41195d7a8219f2faf42a
læuiki <eaa24luba@students.eaaa.dk>
2025-10-06 16:34:27 +0200

    Fix af dataservoce

--- Service/DataService.cs
+++ Service/DataService.cs
@@ -7,51 +7,43 @@ namespace RedditClone.Service;
 public class DataService
 {
     private RedditContent db { get; }
-
     public DataService(RedditContent db)
     {
         this.db = db;
     }
-
     public void SeedData()
     {
-        if (!db.Threads.Any())
+        if (db.Threads.Any()) return;
+        var threads = new List<ThreadsModel>
         {
-            var threads = new List<ThreadsModel>
+            new()
             {
-                new ()
-                {
+                AuthorName = "NoobMaster47",
+                Title = "Hvordan bager man drømmekage?",
+                ThreadsContent = "",
+                Comments = new List<ThreadsCommentsModel>(),
+                UpVotes = 4,
+                DownVotes = 3,
+                Created = DateTime.UtcNow
+            },
 
-                    ThreadsId = 1,
-                    AuthorName = "NoobMaster47",
-                    Title = "Hvordan bager man drømmekage?",
-                    ThreadsContent = "",
-                    Comments = new List<ThreadsCommentsModel>(),
-                    UpVotes = 4,
-                    DownVotes = 3,
-                    Created = DateTime.UtcNow
-                },
+            new()
+            {
+                AuthorName = "Benjaminkorteben",
+                Title = "Hvordan betaler man moms gennem erhverskonto? ",
+                ThreadsContent = "Jeg har lige fået at vide, jeg skal betale moms af dette " +
+                                 "xxxxxxx",
+                Comments = new List<ThreadsCommentsModel>(),
+                UpVotes = 100,
+                DownVotes = 238,
+                Created = DateTime.UtcNow,
+            }
 
-                new()
-                {
-                    ThreadsId = 2,
-                    AuthorName = "Benjaminkorteben",
-                    Title = "Hvordan betaler man moms gennem erhverskonto? ",
-                    ThreadsContent = "Jeg har lige fået at vide, jeg skal betale moms af dette " +
-                                     "xxxxxxx",
-                    Comments = new List<ThreadsCommentsModel>(),
-                    UpVotes = 100,
-                    DownVotes = 238,
-                    Created = DateTime.UtcNow,
-                }
-
-            };        
-            db.Threads.AddRange(threads);
-            db.SaveChanges();
-        }; 
+        };   
+        db.Threads.AddRange(threads);
+        db.SaveChanges();
+        Console.WriteLine($"Seed {threads.Count} threads; ");
     }
-
-
     public async Task<List<ThreadsModel>> GetAllThreads()
     {
         return await db.Threads
@@ -60,22 +52,18 @@ public class DataService
             .Take(50)
             .ToListAsync();
     }
-
     public async Task<ThreadsModel?> GetThreads(int threadsId)
     {
         return await db.Threads
             .Include(t => t.Comments)
             .FirstOrDefaultAsync(t => t.ThreadsId == threadsId);
     }
-
     public async Task<ThreadsModel> CreateThread(ThreadsModel newThreads)
     {
         db.Threads.Add(newThreads);
         await db.SaveChangesAsync();
         return newThreads;
     }
-
-    
     public async Task<ThreadsCommentsModel?> AddComment(int threadId, string content, string authorName)
     {
         var thread = await db.Threads
@@ -95,14 +83,10 @@ public class DataService
         await db.SaveChangesAsync();
         return (comment);
     }
-
-
-    
     public enum VoteType
     {
         Up, Down 
     }
-    
     //____ vote on thread 
     public async Task<bool> VoteThread(int threadId, VoteType voteType)
     {
@@ -117,9 +101,6 @@ public class DataService
         await db.SaveChangesAsync();
         return true;
     }
-    
-    
-    
     //_____ vote on comment 
     public async Task<bool> VoteComment(int commentId, VoteType voteType)
     {
